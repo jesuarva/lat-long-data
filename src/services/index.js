e2b5cb3b9199c57a69613db3c9fe3e85c17050b5
@jesuarva <jesuarva.developer@gmail.com>
2020-04-09 18:04:42 +0100

    Implement starting sunriseSunsetController helper

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,76 @@
+import Pipeline from '@jesuarva/js-pipeline'
 import { ServiceManager } from './ServiceManager'
 
-export const SERVICES = [
-  { uri: 'https://api.sunrise-sunset.org', endpoint: 'json', headers: {} }
-]
+export const SERVICES = {
+  sunriseSunset: {
+    uri: 'https://api.sunrise-sunset.org',
+    endpoint: 'json',
+    headers: {}
+  }
+}
 
-export function InitServices(listOfServices) {
-  return listOfServices.map(({ uri, headers }) => {
-    return ServiceManager().init(uri, headers)
-  })
+export function sunriseSunsetController({
+  points,
+  stats,
+  latLngList,
+  BATCH_NUMBER = 1
+}) {
+  const { uri, headers, endpoint } = SERVICES['sunriseSunset']
+  const service = ServiceManager().init(uri, headers)
+  let requestCount = 0
+  const requests = Array(BATCH_NUMBER)
+
+  Pipeline(latLngList)
+    .effect(() => {
+      if (requestCount % BATCH_NUMBER === 0) {
+        requests.fill(null)
+      }
+      if (
+        requests.length === BATCH_NUMBER &&
+        latLngList.length - requestCount < BATCH_NUMBER
+      ) {
+        requests.length = latLngList.length - requestCount
+      }
+    })
+    .effect(([lat, lng]) => {
+      requests[requestCount % BATCH_NUMBER] = {
+        id: `${lat}-${lng}`,
+        request: service.makeRequest({
+          method: 'get',
+          endpoint,
+          queryParams: { lat, lng }
+        })
+      }
+      requestCount++
+    })
+    .filter(
+      () =>
+        requestCount % BATCH_NUMBER === 0 || requestCount === latLngList.length
+    )
+    .map(() => {
+      return Promise.all(requests.map(({ request }) => request))
+        .then(responses => {
+          return responses.map(({ data }, index) => [requests[index].id, data])
+        })
+        .catch(err => {
+          console.error({ err })
+          return null
+        })
+    })
+    .effect(apiResponses => {
+      if (apiResponses) {
+        console.log(apiResponses)
+        apiResponses.forEach(([pointId, data]) => {
+          console.log({ pointId, data })
+          points.setPointProperty(pointId, {
+            key: 'sunriseSunset',
+            value: data.results
+          })
+        })
+      }
+    })
+    .map(() => {
+      return new Promise(resolve => setTimeout(resolve, 5000))
+    })
+    .runAsync(() => console.log({ points }))
 }
